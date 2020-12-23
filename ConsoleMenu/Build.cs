f5227021e00c7630c1767d241c4a18df8542abd7
Bryan Apellanes <bryan.apellanes@gmail.com>
2020-12-23 10:24:24 -0800

    updated build scripts for app services

---
+++ ConsoleMenu/Build.cs
@@ -0,0 +1,13 @@
+﻿using Bam.Net.CommandLine;
+
+namespace Bam.Net.Application
+{
+    public class Build
+    {
+        [ConsoleAction("install")]
+        public void Install()
+        {
+            
+        }
+    }
+}
--- SemanticAssemblyInfo.cs
+++ SemanticAssemblyInfo.cs
@@ -3,7 +3,7 @@ using Bam.Net;
 
 [assembly: AssemblyVersion("1.0.5.0")]
 [assembly: AssemblyFileVersion("1.0.5.0")]
-[assembly: AssemblyCommit("bda5ffb")]
-[assembly: AssemblySemanticVersion("1.0.5-test+bda5ffb")]
-[assembly: AssemblyDescription("; SemanticVersion=1.0.5-test+bda5ffb, Revision=1.0.5.651979122")]
+[assembly: AssemblyCommit("6f03941")]
+[assembly: AssemblySemanticVersion("1.0.5-test+6f03941")]
+[assembly: AssemblyDescription("; SemanticVersion=1.0.5-test+6f03941, Revision=1.0.5.1340589717")]
 [assembly: AssemblyProduct("bam")]

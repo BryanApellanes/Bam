2b3c53489a0874091f5bf157ff3427736b2bb7c4
Bryan Apellanes bryan.apellanes@okta.com <bryan.apellanes@okta.com>
2021-06-09 09:33:36 -0700

    prep to refactor shell handling

--- SemanticAssemblyInfo.cs
+++ SemanticAssemblyInfo.cs
@@ -3,7 +3,7 @@ using Bam.Net;
 
 [assembly: AssemblyVersion("1.0.5.0")]
 [assembly: AssemblyFileVersion("1.0.5.0")]
-[assembly: AssemblyCommit("f522702")]
-[assembly: AssemblySemanticVersion("1.0.5-test+f522702")]
-[assembly: AssemblyDescription("SemanticVersion=1.0.5-test+f522702, Revision=1.0.5.3762656715")]
+[assembly: AssemblyCommit("d1d6b3d")]
+[assembly: AssemblySemanticVersion("1.0.5-test+d1d6b3d")]
+[assembly: AssemblyDescription("SemanticVersion=1.0.5-test+d1d6b3d, Revision=1.0.5.3241385831")]
 [assembly: AssemblyProduct("bam")]

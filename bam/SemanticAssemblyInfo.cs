70ab8177f70f510a3cf9ae6b5e85447b8f7e1b6e
Bryan <bryan@threeheadz.com>
2022-11-22 16:47:25 -0600

    Bam encryption wip (#2)
    * add X509 certificate generation
    
    Co-authored-by: Bryan Apellanes bryan.apellanes@okta.com <bryan.apellanes@okta.com>

---
+++ bam/SemanticAssemblyInfo.cs
@@ -0,0 +1,9 @@
+using System.Reflection;
+using Bam.Net;
+
+[assembly: AssemblyVersion("1.0.6.0")]
+[assembly: AssemblyFileVersion("1.0.6.0")]
+[assembly: AssemblyCommit("8358330")]
+[assembly: AssemblySemanticVersion("1.0.6-8358330")]
+[assembly: AssemblyDescription("SemanticVersion=1.0.6-8358330, Revision=1.0.6.43617017")]
+[assembly: AssemblyProduct("bam")]
